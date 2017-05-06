f35207b0aa7479e09b3adee7e3dd8c6acddb26ce
a37h <justfastmeow@gmail.com>
2017-05-06 16:10:33 +0300

    new databse structure

--- initalize.sql
+++ initalize.sql
@@ -2,36 +2,30 @@ CREATE TABLE Users (
     user_id serial PRIMARY KEY,
     user_name text UNIQUE NOT NULL,
     user_email text UNIQUE NOT NULL,
-    user_password varchar(100) NOT NULL
-);
-
-CREATE TABLE Sessions_log (
-	session_id serial PRIMARY KEY,
-    user_id int NOT NULL REFERENCES Users (user_id),
-	type boolean NOT NULL,
-	time_stamp timestamp NOT NULL
-);
-
-CREATE TABLE User_expirience (
-    user_id int PRIMARY KEY REFERENCES Users (user_id),
-	total_time_spent real NOT NULL,
-	amount_of_finished_sessions real NOT NULL
-);
-
-CREATE TABLE Mailing_list (
-    user_id int PRIMARY KEY REFERENCES Users (user_id),
-	email_sub_agreement boolean NOT NULL,
-	last_notification timestamp,
-	notfication_clickthrough_rate real
+    user_password text NOT NULL,
+	  email_sub_agreement boolean NOT NULL,
 );
 
 CREATE TABLE Groups (
 	group_id serial PRIMARY KEY,
-	type boolean NOT NULL, 
+	alive_status boolean NOT NULL,
 	group_name text UNIQUE NOT NULL
 );
 
 CREATE TABLE User_groups (
 	group_id int NOT NULL REFERENCES Groups (group_id),
 	user_id int NOT NULL REFERENCES Users (user_id)
+);
+
+CREATE TABLE Entitys (
+  entity_id serial PRIMARY KEY,
+  type boolean NOT NULL,
+  entity_origin_id int UNIQUE NOT NULL
+);
+
+CREATE TABLE Sessions_log (
+	session_id serial PRIMARY KEY,
+  entity_id int NOT NULL REFERENCES Entitys (entity_id),
+	type boolean NOT NULL,
+	time_stamp timestamp NOT NULL
 );
