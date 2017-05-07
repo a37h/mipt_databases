176d0595f1c36c686ec778b69927128b3b4e2aff
a37h <justfastmeow@gmail.com>
2017-05-07 15:36:05 +0300

    nothing

--- App/main.py
+++ App/main.py
@@ -16,14 +16,28 @@ def main():
     db_connection = db_connect()
     db_cursor = db_connection.cursor()
 
-    register(db_cursor, 'Anatoly', 'Anatoly@mail.ru', '123', 'True')
+    register(db_cursor, 'abb', 'bbb', 'cbb', 'True')
+    db_connection.commit()
+    get_all_users(db_cursor)
+    db_connection.commit()
 
 
 def register(db_cursor, name, email, password, email_subscrition):
     try:
         db_cursor.execute("INSERT INTO Users (user_name, user_email, user_password, email_sub_agreement) "
-                          "values ('%s', '%s', '%s', '%s');" % "Anatoly" "Anatoly@mail.ru" "123" "True")
+                          "values ('%s', '%s', '%s', '%s');" % (name, email, password, email_subscrition))
     except psycopg2.Error as e:
         print("Error registering user. Error:", e)
+
+
+def get_all_users(db_cursor):
+    try:
+        db_cursor.execute("SELECT * FROM Users;")
+        user_list = db_cursor.fetchall()
+        for i in user_list:
+            print(i)
+    except psycopg2.Error as e:
+        print("Error fetching users. Error: ", e)
+
 
 main()
