d6360b2ca1d9a4c56cb8e8bb79abafe003a73e60
a37h <justfastmeow@gmail.com>
2017-05-08 04:28:11 +0300

    it's so cool u knoww

--- App/main.py
+++ App/main.py
@@ -1,6 +1,7 @@
 import psycopg2
 import psycopg2.extras
-
+import os
+import getpass
 
 def db_connect():
     connection_string = 'dbname=mipt_db_project user=mipt_db_project_user ' \
@@ -17,6 +18,8 @@ def main():
     db_connection = db_connect()
     db_cursor = db_connection.cursor()
 
+    current_user_info = []
+
     print("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓")
     print("┣━━━━━ TimeManagementApp3000 ━━━━━━━━━━━━━━┫")
     print("┣━ 'help' or 'h' to get a list of commands ┫")
@@ -25,62 +28,119 @@ def main():
 
     while True:
         try:
-            input_line = input('➤ ')
-            parser = {'r': frontend_user_registration,
-                      'register': frontend_user_registration,
-                      'l': frontend_user_login,
-                      'login': frontend_user_login,
-                      'h': frontend_help,
-                      'help': frontend_help}
+            input_line = input('┣ ➤  ')
+
+            if input_line == 'h' or input_line == 'help':
+                frontend_show_help(current_user_info)
+
+            elif input_line == 'r' or input_line == 'register':
+                if len(current_user_info) == 0:
+                    current_user_info = frontend_user_registration(db_cursor)
+                else:
+                    print("┣━━━━━ Error: you are already logged in. Use 'h' or 'help'")
+
+            elif input_line == 'l' or input_line == 'login':
+                if len(current_user_info) == 0:
+                    current_user_info = frontend_user_login(db_cursor)
+                else:
+                    print("┣━━━━━ Error: you are already logged in. Use 'h' or 'help'")
+
+            elif input_line == 'q' or input_line == 'quit':
+                shutdown_app()
+
+            elif input_line == 'o' or input_line == 'logout':
+                if len(current_user_info) == 0:
+                    print("┣━━━━━ Error: you aren't logged in. Use 'l' or 'login'")
+                else:
+                    print("┣━━━━━ Goodbye, %s" % (current_user_info[0],))
+                    current_user_info = []
+
+            elif input_line == 'clear' or input_line == 'c':
+                os.system('cls' if os.name == 'nt' else 'clear')
+
+            else:
+                print("┣━━━━━ Error: no such command, try again or use 'h' or 'help'")
+
         except EOFError:
-            print()
-            exit(0)
-        try:
-            activate = parser[input_line]
-            activate(db_cursor)
-        except KeyError:
-            print("┣━━━━┫ Error: no such command, try again or use 'h' or 'help'")
+            shutdown_app()
 
 
-def frontend_help(*args):
-    print("┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓")
-    print("┣━ If you're new then register by typing 'r' or 'register' ━┫")
-    print("┣━ To login with your name and password type 'l' or 'login' ┫")
-    print("┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛")
+def shutdown_app():
+    print()
+    print('┗━━━━━━━━━━ Peace out ━━━ ◠ ◡ ◠  ━━━━━━━━━━━━━━━')
+    print()
+    exit(0)
 
 
-def frontend_user_login(db_cursor):
-    name =     input('┣━━━━┫ Login: ')
-    password = input('┣━━━━┫ Password: ')
-    backend_user_login(db_cursor, name, password)
-
-
-def frontend_user_registration(db_cursor):
-    name = input('┣━━━━┫ Please enter your login: ')
-    email = input('┣━━━━┫ Please enter your email: ')
-    password = input('┣━━━━┫ Please enter your password: ')
-    email_subscription = str(input('┣━━━━┫ Email subscription? (y\\n): '))
-    if email_subscription == 'y':
-        email_subscription = True
+def frontend_show_help(current_user_info):
+    if len(current_user_info) == 0:
+        print("┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓")
+        print("┣━━━━━━━ First you have to login into your account ━━━━━━━━━┫")
+        print("┣━ 'r' or 'register' to register if you're new ━━━━━━━━━━━━━┫")
+        print("┣━ 'l' or 'login' to login if you're already registered ━━━━┫")
+        print("┣━ 'c' or 'clear' if you want to clear your screen ━━━━━━━━━┫")
+        print("┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛")
     else:
-        email_subscription = False
-    backend_register_new_user(db_cursor, name, email, password, email_subscription)
+        print("┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓")
+        print("┣━━━━━━━━━━━ List of commands available for you ━━━━━━━━━━━━┫")
+        print("┣━ 'c' or 'clear' if you want to clear your screen ━━━━━━━━━┫")
+        print("┣━ 'o' or 'logout' to logout from your account ━━━━━━━━━━━━━┫")
+        print("┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛")
 
 
+# Login (first part)
+def frontend_user_login(db_cursor):
+    try:
+        try:
+            name =     input('┣━━━━━ Login: ')
+            password = getpass.getpass('┣━━━━━ Password: ')
+            return backend_user_login(db_cursor, name, password)
+        except KeyboardInterrupt:
+            print()
+            return []
+    except EOFError:
+        shutdown_app()
+
+
+# Login (second part)
 def backend_user_login(db_cursor, name, password):
     try:
         sql_string = "SELECT * FROM Users WHERE user_name = %s AND user_password = %s"
         sql_data_tuple = (name, password)
         db_cursor.execute(sql_string, sql_data_tuple)
     except psycopg2.Error as e:
-        print("┣━━━━┫ Error loggining. Error:", e)
+        print("┣━━━━v Error loggining. Error:", e)
     result = db_cursor.fetchall()
     if len(result) == 1:
-        print('┣━━━━┫ Success. Welcome, %s' % name)
+        print('┣━━━━━ Success. Welcome, %s' % name)
+        new_user_id = backend_get_user_id(db_cursor, name)
+        return [name, new_user_id]
     else:
-        print('┣━━━━┫ Error: such user doesn\'t exist or password is wrong')
+        print('┣━━━━━ Error: such user doesn\'t exist or password is wrong')
+        return ''
 
 
+# Registration (first part)
+def frontend_user_registration(db_cursor):
+    try:
+        try:
+            name = input('┣━━━━━ Please enter your login: ')
+            email = input('┣━━━━━ Please enter your email: ')
+            password = getpass.getpass('┣━━━━━ Please enter your password: ')
+            email_subscription = str(input('┣━━━━━ Email subscription? (y\\n): '))
+            if email_subscription == 'y':
+                email_subscription = True
+            else:
+                email_subscription = False
+            return backend_register_new_user(db_cursor, name, email, password, email_subscription)
+        except KeyboardInterrupt:
+            print()
+            return []
+    except EOFError:
+        shutdown_app()
+
+
+# Registration (second part)
 def backend_register_new_user(db_cursor, name, email, password, email_subscrition):
     try:
         sql_string = "INSERT INTO Users (user_name, user_email, user_password, " \
@@ -88,46 +148,25 @@ def backend_register_new_user(db_cursor, name, email, password, email_subscritio
         sql_data_tuple = (name, email, password, email_subscrition)
         db_cursor.execute(sql_string, sql_data_tuple)
 
-        sql_string = "SELECT user_id FROM Users WHERE user_name = %s"
-        sql_data_tuple = (name,)
-        db_cursor.execute(sql_string, sql_data_tuple)
-
-        new_user_id_list = db_cursor.fetchall()
-        new_user_id = new_user_id_list[0]
+        new_user_id = backend_get_user_id(db_cursor, name)
 
         sql_string = "INSERT INTO Entitys (type, user_id) VALUES (%s, %s)"
         sql_data_tuple = (False, new_user_id)
         db_cursor.execute(sql_string, sql_data_tuple)
 
-        print("┣━━━━┫ Success. Now you can login with your login and password")
+        print("┣━━━━━ Success. You are now logged in,", name)
+        return [name, new_user_id]
     except psycopg2.Error as e:
-        print("┣━━━━┫ Error registering user. Error:", e)
+        print("┣━━━━━ Error registering user. Error:", e)
+        return []
 
 
-def frontend_show_a_list_of_users(db_cursor, name):
-    try:
-        print("┣━━━━┫ Retrieving list of all users...")
-        db_cursor.execute("SELECT * FROM Users;")
-        user_list = db_cursor.fetchall()
-    except psycopg2.Error as e:
-        print("┣━━━━┫ Error fetching users. Error: ", e)
-    if name == 'Superuser':
-        for i in user_list:
-            print(i)
-    else:
-        for i in user_list:
-            print(i[1])
-
-
-def frontend_show_a_list_of_entitys(db_cursor, name):
-    try:
-        print("┣━━━━┫ Retrieving list of all entitys...")
-        db_cursor.execute("SELECT * FROM Entitys;")
-        entitys_list = db_cursor.fetchall()
-        for i in entitys_list:
-            print(i)
-    except psycopg2.Error as e:
-        print("┣━━━━┫ Error fetching entitys. Error: ", e)
+def backend_get_user_id(db_cursor, name):
+    sql_string = "SELECT user_id FROM Users WHERE user_name = %s"
+    sql_data_tuple = (name,)
+    db_cursor.execute(sql_string, sql_data_tuple)
+    new_user_id_list = db_cursor.fetchall()
+    return new_user_id_list[0]
 
 
 main()
